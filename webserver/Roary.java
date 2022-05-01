3ed5b197b15b7f7cd1587da748fd345ca65fdb5e
Sebastian Boll <sebastian.boll@doubleslash.de>
2022-05-01 17:04:03 +0200

    Add functionality to insert roars via the main cgi script

--- webserver/Roary.java
+++ webserver/Roary.java
@@ -1,7 +1,12 @@
 package webserver;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +33,7 @@ public class Roary {
         try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
-
+            roar = new Roar("test","","");
             while (line != null) {
                 sb.append(line);
                 sb.append(System.lineSeparator());
@@ -58,11 +63,61 @@ public class Roary {
         return htmlOutput;
     }
 
+    static void addNewRoar(){    
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+        String data = sb.toString();
+
+        String[] strings = data.split("&");
+        String name = "";
+        String message = "";
+
+        String[] namePair = strings[0].split("=");
+        if (namePair[0].equals("name")){
+            name = namePair[1];
+        }
+
+        String[] msgPair = strings[1].split("=");
+        if (msgPair[0].equals("message")){
+            message = msgPair[1];
+        }
+
+        String roar = name + "," + message + "," + LocalDateTime.now().toString();
+        //remove random linebreaks from datetime
+        roar = roar.replace("\n", "").replace("\r", "");
+        // replace + with spaces
+        roar = roar.replace("+", " ");
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("data/roars.csv", true));
+        writer.append('\n');
+        writer.append(roar);
+        
+        writer.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+
+    }
     public static void main(String[] args) {
         String htmlUpper = readTemplate("html/template-upper.html");
         String htmlLower = readTemplate("html/template-lower.html");
-        List<List<String>> oldRoars = loadRoars("data/roars.csv");
-        String roarsHtml = generateHtmlFromRoars(oldRoars);
+
+        String method = System.getenv("REQUEST_METHOD");
+        if (method.equals("POST")){
+            addNewRoar();
+        }
+
+        List<List<String>> allRoars = loadRoars("data/roars.csv");
+        List<List<String>> all = allRoars.sort((l1, l2) -> l1.get(2).compareTo(l2.get(2)));
+        String roarsHtml = generateHtmlFromRoars(allRoars);
+
         System.out.print("Content-Type: text/html\r\n\r\n");
         System.out.println(htmlUpper);
         System.out.println(roarsHtml);
